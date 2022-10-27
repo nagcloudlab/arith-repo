002c64e0022c539ff6abb15f994eece0315a06bd
nag <nagcloudlab@gmail.com>
2022-10-27 16:30:34 +0530

    arith-service with add()

---
+++ arith-service/src/main/java/com/example/ArithServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithServiceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArithServiceApplication.class, args);
+    }
+
+}
---
+++ arith-service/src/main/java/com/example/web/ArithController.java
@@ -0,0 +1,21 @@
+package com.example.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/arith")
+public class ArithController {
+
+    @GetMapping("/add/{n1}/{n2}")
+    public ResponseEntity<String> add(
+            @PathVariable("n1") int n1,
+            @PathVariable("n2") int n2) {
+        int result = n1 + n2;
+        return ResponseEntity.ok(String.valueOf(result));
+    }
+
+}
